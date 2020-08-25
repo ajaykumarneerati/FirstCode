668d6981e66cd3c84b054ec5fefa1e74afd6f278
satyanarayan-neerati <satyanarayana.neerati@thermofisher.com>
2020-08-25 12:09:41 -0400

    Initial Commit

---
+++ FirstCode/src/com/main/Variables.java
@@ -0,0 +1,36 @@
+package com.main;
+
+public class Variables {
+	
+	public static void main (String[] args) {
+		
+/*		String firstName= "john ";
+		String lastName = "Kaneria";
+		String fullName = firstName + lastName;
+		System.out.println (fullName);
+//		System.out.println("Hello "+LastName);
+	//	System.out.println(FirstName + " " + LastName); */
+		
+/*		int num=10;
+		int x= 20;
+		int z=43;
+		
+		System.out.println(num+x+z); */
+		
+/*		int x= 34;
+		x=32;
+		System.out.println(x); */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
