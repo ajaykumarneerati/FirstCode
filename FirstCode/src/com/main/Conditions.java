862c5333722aa293b74afe3eec0e10f1350416a3
satyanarayan-neerati <satyanarayana.neerati@thermofisher.com>
2020-08-25 20:52:19 -0400

    Conditions.Java file has been added

---
+++ FirstCode/src/com/main/Conditions.java
@@ -0,0 +1,25 @@
+package com.main;
+
+public class Conditions {
+	
+	public static void main (String[] args) {
+		
+		int time = 12;
+		
+		if (time < 10) {
+			
+			System.out.println("Good Morning");
+		}
+		
+		else if (time < 9) {
+			
+			System.out.println("Good Afternoon");
+		}
+		
+		else {
+			
+			System.out.println("Good Night");
+		}
+	}
+
+}
